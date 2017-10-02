a189b5d4441ec18f58a5be425dff2778008070f2
Guilherme Garcia <grgguilherme@gmail.com>
2017-10-02 17:42:46 -0300

    Backend | WIP - Initial server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.port || 3001;
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json ({
+        "message": "OK!"
+    });
+});
+
+router.use('products', router);
+
+app.listen(port);
+console.log("Listening on " + port);
