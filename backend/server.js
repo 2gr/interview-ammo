4bba1dab4ba8bfae05941378b57269078670e7b6
Guilherme Garcia <grgguilherme@gmail.com>
2017-10-03 17:14:32 -0300

    Backend | WIP - Really crude backend

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,31 @@ var port = process.env.port || 3001;
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', async function(req, res) {
+
+    const Sequelize = require('sequelize');
+    var sequelize = new Sequelize('ammo_teste', 'harry', 'senhateste', {dialect: 'postgres'});
+
+    const Product = sequelize.define(
+        'products', {
+            id: {type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true},
+            name: {type: Sequelize.STRING, allowNull: false}
+        }
+    );
+
+    var query = req.query.query || 'Lençol';
+    var page = req.query.page || '1';
+    var products = await Product.findAndCountAll({
+        where: {name: {$ilike: '%teste%'}},
+        limit: 20,
+        offset: 0
+    });
+
     res.json ({
-        "message": "OK!"
+        "message": "OK!",
+        "query" : query,
+        "page": page,
+        "data": products
     });
 });
 
